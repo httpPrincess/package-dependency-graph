882e38959ba7e8937e26bad67e533f568d73881b
httpPrincess <j.rybicki@fz-juelich.de>
2015-07-08 14:22:38 +0200

    formatting

--- ex.py
+++ ex.py
@@ -6,30 +6,30 @@ import sys
 from subprocess import Popen, PIPE
 import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler())
+LOG = logging.getLogger(__name__)
+LOG.setLevel(logging.INFO)
+LOG.addHandler(logging.StreamHandler())
 
 
 def clean_graph(graph):
-    logger.info('Cleaning up graph...')
+    LOG.info('Cleaning up graph...')
     try:
         graph.cypher.execute('MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r')
         graph.schema.drop_uniqueness_constraint('Package', 'name')
     except GraphError as error:
-        logger.error('Problem cleaning up graph %r', error)
+        LOG.error('Problem cleaning up graph %r', error)
 
 
 def get_graph(cleanup=False):
     uri = getenv('NEO4J_URI', 'http://neo4j:neo@localhost:7474/db/data/')
-    logger.info('Connecting to graph at: %s', uri)
+    LOG.info('Connecting to graph at: %s', uri)
     graph = Graph(uri)
     if cleanup:
         clean_graph(graph)
     try:
         graph.schema.create_uniqueness_constraint('Package', 'name')
     except GraphError as error:
-        logger.error(
+        LOG.error(
             'Unable to create constrains (they already exist perhaps?) %r',
             error)
         return None
@@ -49,18 +49,18 @@ def process_package(graph, package):
         if dep_name is None:
             continue
         dep = graph.merge_one('Package', 'name', dep_name)
-        logger.info('(%s)-[:DEPENDS_ON]->(%s)', package['name'], dep_name)
+        LOG.info('(%s)-[:DEPENDS_ON]->(%s)', package['name'], dep_name)
         graph.create_unique(Path(p, 'DEPENDS_ON', dep))
 
 
-def parse_package_list(input):
-    packages = []
-    for l in input:
+def parse_package_list(package_list):
+    ret = []
+    for l in package_list:
         try:
-            packages.append(json.loads(l))
-        except Exception as inst:
-            logger.error('Error processing %s %s', l, inst)
-    return packages
+            ret.append(json.loads(l))
+        except ValueError as inst:
+            LOG.error('Error processing %s %s', l, inst)
+    return ret
 
 
 def get_packages_from_file(fname):
@@ -79,29 +79,28 @@ def generate_package_list():
     process = Popen(command, stdout=PIPE)
     out, err = process.communicate()
     if err:
-        logger.error('Unable to execute command %s [error=%s] %s',
-                      command, err, out)
+        LOG.error('Unable to execute command %s [error=%s] %s',
+                  command, err, out)
     else:
         return parse_package_list(out.splitlines())
 
 
 if __name__ == "__main__":
-
     fname = 'out.json'
     if len(sys.argv) > 1:
         fname = sys.argv[1]
     if path.isfile(fname):
-        logger.info('Retrieving package list from %s', fname)
+        LOG.info('Retrieving package list from %s', fname)
         packages = get_packages_from_file(fname)
     else:
-        logger.info('Generating package list with dpkg-query')
+        LOG.info('Generating package list with dpkg-query')
         packages = generate_package_list()
 
     if packages is None or len(packages) == 0:
-        logger.error('Unable to create package list')
+        LOG.error('Unable to create package list')
         exit(-1)
 
-    logger.info('Got %d packages' % len(packages))
+    LOG.info('Got %d packages', len(packages))
     g = get_graph(cleanup=False) or exit(-1)
     map(partial(process_package, g), packages)
     g.push()
